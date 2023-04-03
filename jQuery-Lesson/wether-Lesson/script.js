5739cc2a0461f8a147fba3c550b0dce105db1c6d
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-04-03 11:36:47 +0000

    JavaScript,API勉強中

--- jQuery-Lesson/wether-Lesson/script.js
+++ jQuery-Lesson/wether-Lesson/script.js
@@ -1,11 +1,11 @@
 // （API_KEY には、"取得したAPIキー" を記述）
-const API_KEY = "取得したAPIキー";
+const API_KEY = "1300fb9a8b10287f90e583f58867d479";
 
 $(function(){
   $('#btn').on('click', function() {
     // 入力された都市名でWebAPIに天気情報をリクエスト
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "1300fb9a8b10287f90e583f58867d479",
       dataType : 'jsonp',
     }).done(function (data){
       //通信成功
@@ -25,7 +25,7 @@ $(function(){
       $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
       $('img').attr("alt",data.weather[0].main);    }).fail(function (data) {
       //通信失敗
-      alert('通信に失敗しました。')
+      alert('通信に失敗しました。');
     });
   });
 });
