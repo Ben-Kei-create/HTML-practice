6d8d090fa8cbba018b0a39aa8c8dfa96d245f887
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-05-08 10:16:07 +0000

    Study休憩

--- java/sample.java
+++ java/sample.java
@@ -9,7 +9,7 @@ public class sample {
 
 double i = 2.5;
 System.out.println(i);
-// (1)
+// (1)“Hello World”と表示するプログラムを作成しなさい。
 public class sample
 {
   public static void main(String[] args)
@@ -25,4 +25,29 @@ public class Exercise
         System.out.println("Hello World");
     }
 }
+// int 型の変数 x に数値 11 を代入し、x の値を“x=11”のように表示するプログラムを作成しなさい。
 
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        int x = 11;
+        System.out.println("x=" + x );
+    }
+}
+
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        int x;
+
+        x = 11;
+
+        System.out.println( "x=" + x );
+    }
+}
+
+// int 型の変数 x、y に数値 13、17 を代入し、x、y の値を“x=13,y=17”のように表示するプログラムを作成しなさい。
+
+public class 
