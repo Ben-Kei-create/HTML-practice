aa1fce41c0e5e835849c0a643a8c53c79e9bd6e5
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-05-08 14:34:36 +0000

    Study開始

--- java/sample.java
+++ java/sample.java
@@ -49,5 +49,113 @@ public class Exercise
 }
 
 // int 型の変数 x、y に数値 13、17 を代入し、x、y の値を“x=13,y=17”のように表示するプログラムを作成しなさい。
+public class Exercise{
+  public static void main(String[] args){
+    int x = 13;
+    int y = 17;
+    System.out.println( "x="+ x + ",y=" + y );
+  }
+}
 
-public class 
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        int x;
+        int y;
+
+        x = 13;
+        y = 17;
+
+        System.out.println( "x=" + x + ",y=" + y );
+    }
+}
+
+// 練習問題 1 - 4 int 型の変数 x に数値 13 と 17 の和を代入し、x の値を表示するプログラムを作成しなさい。
+
+public class Exercise{
+  public static void main(String[] args){
+    int x = 13 + 17;
+    System.out.println(x);
+  }
+}
+
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        int x;
+
+        x = 13 + 17;
+
+        System.out.println( "x=" + x );
+    }
+}
+
+// 練習問題 1 - 5数値 13 と 17 の積を表示するプログラムを作成しなさい。ただし、変数を使用しないこと。※ 	System.out.println()の引数部分で演算を行う。
+
+public
+class Exercise
+{
+  public static void main(String[] args)
+  {
+    int x = 13;
+    int y = 17;
+    System.out.println(x*y);
+  }
+}
+
+
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        System.out.println( 13 * 17 );
+    }
+}
+
+// 練習問題 1 - 6
+// 次のプログラムを作成しなさい。
+// int 型の変数 x に数値 7 を代入する。
+// 変数 x の値を 3 倍にする。
+// 変数 x の値を表示する。
+// 変数 x の値を半分（1/2）にする。
+// 変数 x の値を表示する。
+
+
+public class excise
+{
+  public static void main(String[] args)
+  {
+    int x;
+    x = 7;
+    System.out.println(x*3);
+    System.out.println(x/2);
+  }
+}
+
+
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        int x;
+
+        x = 7;
+        x *= 3;
+        System.out.println( "x=" + x );
+        x /= 2;
+        System.out.println( "x=" + x );
+    }
+}
+
+// int 型の変数 x、y に任意の数値を代入し、x の値を y に、y の値を x に入れ替えて x と y の値を表示するプログラムを作成しなさい。
+
+// ※ 	xとy以外に、もうひとつ変数が必要となる。
+// x = 3;
+// y = 7;
+
+// // ここでxとyの値を入れ替える
+
+// System.out.println( "x=" + x + ",y=" + y );
+
