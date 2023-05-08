9410b725322aba09fa331608aa7b2b508b03b79e
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-05-08 10:13:02 +0000

    Study

---
+++ java/sample.java
@@ -0,0 +1,28 @@
+public class sample {
+  public static void main(String[] args){
+    System.out.println("Javaプログラムです。");
+    System.out.println("ただいま");
+  }
+}
+
+// メソッド名（引数, 引数,...）;
+
+double i = 2.5;
+System.out.println(i);
+// (1)
+public class sample
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
+
+public class Exercise
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World");
+    }
+}
+
